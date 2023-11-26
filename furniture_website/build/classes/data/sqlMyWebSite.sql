5cfd72940bc4fe73cf397d86b2cbc9ec7ba66160
Lê Thanh Tâm <tam1442k3@gmail.com>
2023-11-26 11:42:25 +0700

    Update sqlMyWebSite.sql

--- furniture_website/build/classes/data/sqlMyWebSite.sql
+++ furniture_website/build/classes/data/sqlMyWebSite.sql
@@ -202,7 +202,8 @@ insert into GroupPro(groupID,name,content) values
 ('gr001','Do Noi That',''),
 ('gr002','Do Trang Tri',''),
 ('gr003','Thiet Bi Ve Sinh',''),
-('gr004','Den Trang Tri','');
+('gr004','Den Trang Tri',''),
+('gr005','San Pham Thu Cong','');
 
 
 ----Category table
@@ -222,11 +223,29 @@ insert into Category(categoryID,name,content) values
 ('c013','Den chum',''),
 ('c014','Den am tran',''),
 ('c015','Den tha tran',''),
-('c016','Den cay-Den ban','');
+('c016','Den cay-Den ban',''),
+('c017','San pham thu cong','');
 
 ----Product table
 insert into product(productID,productName,categoryID,groupID,wattage,producer,
-packeged,brandOrigin,meterial,type,color,thicknessWood,space,design,timeToUse,price,decription,status,qualityInStorage) values
+packeged,brandOrigin,meterial,size,type,color,thicknessWood,space,design,timeToUse,price,decription,status,qualityInStorage) values
 	('p001','Den chum','c013','gr004','Cam ung khong cham,tang giam do cao','Bean Violight','1 den','Trung Quoc',
-	'Kim loai son tinh dien + Mika','decoration','Vang','','','','',1750000.0,
-	'thiet ke don gian,hien dai, tien nghi, cung do ben cao','',30);
+	'Kim loai son tinh dien + Mika','','Den Trang Tri','Vang','','','','',1750000.0,
+	'thiet ke don gian,hien dai, tien nghi, cung do ben cao','',30),
+
+	('p002','Don luc binh dan thu cong','c017','gr005','','Bean Oem','','Viet Nam',
+	'Luc binh ket hop coi va be ngo','','San Pham Thu Cong','Tu nhien','','','Nho gon, de dang di chuyen','Lau den 3-5 nam',500000.0,
+	'Dung lam ghe ngoi hoac ban dung do trang tri phong khach, mau sac tu nhien tao khong gian thoang mat gan gui','',10),
+
+	('p003','Ke giay 6 tang ','c005','gr002','','No Brand','Phu kien kem theo: 1 giay huong dan lap rap va 1 bo oc dung de lap dat','Viet Nam',
+	'Go MDF','','Do Trang Tri','Nau','18mm','','','',1000000.0,
+	'Co suc chua 18 doi giay, con co the su dung de trung bay sach doc, cay canh,lo hoa, chong tray va chong am cao','',20),	
+
+	('p004','Chau rua mat Lavabo dat ban ','c012','gr003','','Bean Toto','Bao gom voi xa va bo xa','Hoa Ky',
+	'Tam SPCC va chup den PMMA','','Thiet Bi Ve Sinh','Trang','','Dat noi tren ban','','',3500000.0,
+	'Kieu dang doc dao,hien dai. Thiet ke voi lo thoat tran dang trang tri','',15),
+
+	('p005','Ghe an Basil HomeLand','c002','gr001','','HomeLand','','Viet Nam',
+	'Thep boc PVC/PU','Kich thuoc:460x610x980mmH','Do Noi That','Den,Nau','','','','',750000.0,
+	'Thiet ke theo kieu dang co ban, co gam mau phu hop voi khong gian noi that','',20);
+
