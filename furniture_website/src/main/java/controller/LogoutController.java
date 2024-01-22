3a350a683f761e39a8072c6f27dcc05cca0d03d5
TaiVo111 <137768106+TaiVo111@users.noreply.github.com>
2024-01-22 13:44:54 +0700

    value, xulylogin

--- furniture_website/src/main/java/controller/LogoutController.java
+++ furniture_website/src/main/java/controller/LogoutController.java
@@ -1,3 +1,5 @@
+package controller;
+
 
 import java.io.IOException;
 import javax.servlet.ServletException;
@@ -10,21 +12,16 @@ import javax.servlet.http.HttpSession;
 /**
  * Servlet implementation class LogoutController
  */
-@WebServlet("/LogoutController")
+@WebServlet(name="/LogoutController", urlPatterns = {"/logout"})
 public class LogoutController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		HttpSession session = request.getSession();
-		session.invalidate();
-		response.sendRedirect("index.jsp");
-	}
-
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		HttpSession session = request.getSession();
+		session.removeAttribute("acc");
+		response.sendRedirect("index.jsp");
 	}
+	
 
 }
